db98900edc21808dca8a875e43016d7a1695b66c
Adam Teo <adam.teo321@gmail.com>
2022-12-21 22:39:17 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mapImage from "./assets/map.svg";
+import normalLevel from "./assets/normal-level.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="room__container">
+
+      <div className="room__container-header">
+        <h1>Untitled Room</h1>
+      </div>
+
+      <div className="room__container-content">
+
+        <div className="room__container-content_left">
+
+          <div className="room__container-content_left-top">
+            <div className="room__container-players">
+              <div className="room__container-players_header">
+                <h3>Players</h3>
+              </div>
+              <div className="room__container-players_content">
+                <p>⬤ POOLED (host) <br /> ⬤ jas</p>
+              </div>
+            </div>
+
+            <div className="room__container-spectators">
+              <div className="room__container-spectators_header">
+                <h3>Spectators</h3>
+              </div>
+              <div className="room__container-players_content">
+                <p>⬤ GMHikaru <br /> ⬤ shigetora <br /> ⬤ mrekk</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="room__container-map">
+            <div className="room__container-map_header">
+              <h3>Map</h3>
+            </div>
+            <div className="room__container-map_content">
+              <div className="room__container-map_content-image">
+                <img src={mapImage} alt="map" />
+              </div>
+              <div className="room__container-map_content-description">
+                <h4>8 Doors - POOLED</h4>
+                <p>recommended players: 1-2 <br /> difficulty: ●○○○</p>
+              </div>
+              <div className="room__container-map_content-type">
+                <img src={normalLevel} alt="normal-level" />
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+        <div className="room__container-chat">
+          <div className="room__container-chat_header">
+            <h3>Chat</h3>
+
+          </div>
+          <div className="room__container-chat_content">
+            <div className="room__container-chat_history">
+              <p>POOLED: hello <br /> jas: hi <br /> POOLED: how are you? <br /> jas: good <br /> POOLED: nice <br /> jas: thanks <br /> POOLED: no problem <br /> jas: bye <br /> POOLED: bye</p>
+            </div>
+            <div className="room__container-chat_input">
+              <input type="text" placeholder="Type a message..." />
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="room__container-buttons">
+        <button>
+          start
+        </button>
+        <button>
+          spectate
+        </button>
+        <button>
+          leave
+        </button>
+      </div>
+
     </div>
   );
 }
