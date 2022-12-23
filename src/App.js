e77cdefbce1d5b53abffa5a9290a31a31fd2a629
Adam Teo <adam.teo321@gmail.com>
2022-12-22 22:45:46 -0600

    Create reusable UserList component for players and spectators

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 
 import ChatBox from "./components/ChatBox/ChatBox";
 import MapSelect from "./components/MapSelect/MapSelect";
+import UserList from "./components/UserList/UserList";
+
+const players = ["POOLED (host)", "jas"]
+const spectators = ["GMHikaru", "shigetora", "mrekk"]
 
 function App() {
   return (
@@ -12,35 +16,14 @@ function App() {
       </div>
 
       <div className="room__container-content">
-
         <div className="room__container-content_left">
-
           <div className="room__container-content_left-top">
-            <div className="room__container-players">
-              <div className="room__container-players_header">
-                <h3>Players</h3>
-              </div>
-              <div className="room__container-players_content">
-                <p>⬤ POOLED (host) <br /> ⬤ jas</p>
-              </div>
-            </div>
-
-            <div className="room__container-spectators">
-              <div className="room__container-spectators_header">
-                <h3>Spectators</h3>
-              </div>
-              <div className="room__container-players_content">
-                <p>⬤ GMHikaru <br /> ⬤ shigetora <br /> ⬤ mrekk</p>
-              </div>
-            </div>
+            <UserList type="Players" dummyUsers={players} />
+            <UserList type="Spectators" dummyUsers={spectators} />
           </div>
-
           <MapSelect />
-
         </div>
-
         <ChatBox />
-
       </div>
 
       <div className="room__container-buttons">
---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './UserList.css'
+
+const userDot = "⬤ ";
+
+const UserList = ({ type, dummyUsers }) => {
+  return (
+    <div className="room__userlist" style={{
+      marginRight: (type === "Players") ? "1rem" : "none",
+      marginLeft: (type === "Spectators") ? "1rem" : "none"
+    }}>
+      <div className="room__userlist-header">
+        <h3>{type}</h3>
+      </div>
+      <div className="room__userlist-content">
+        <p>
+          {dummyUsers.map((user) => (
+            <>
+              {userDot}{user}
+              <br />
+            </>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default UserList;
