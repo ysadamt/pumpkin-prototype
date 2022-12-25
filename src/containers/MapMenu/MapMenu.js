a4d237e2769f0cdec73000a2a9008f7c3b32e470
Adam Teo <adam.teo321@gmail.com>
2022-12-24 21:43:50 -0600

    Add close button in level selection window

--- src/containers/MapMenu/MapMenu.js
+++ src/containers/MapMenu/MapMenu.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./MapMenu.css";
 import normalMode from "../../assets/gamemodes/normal.svg";
 import freezeMode from "../../assets/gamemodes/freeze.svg";
+import Button from "../../components/Button/Button";
+import "./MapMenu.css";
 
 const MapMenu = (props) => {
   return (
     <div
-      className={props.hide ? "hidden" : "scrollable window"}
+      className={props.hide ? "hidden window" : "scrollable window"}
       style={{ zIndex: 1 }}
     >
       <div className="mapMenu__container">
+        <div className="mapMenu__container-close">
+          <Button name="close" handleClick={() => props.onChoose()}/>
+        </div>
         <div className="mapMenu__container-header">
           <h1>level select</h1>
         </div>
